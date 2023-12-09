fc69695d76eb3ce2954c1de8579b66a9823a19b4
Damian Walendzik <Damianwalendzik7@gmail.com>
2023-12-09 12:40:37 +0100

    Logging added

--- main.py
+++ main.py
@@ -1,65 +1,80 @@
-import os, re, shutil
+import os, re, shutil, logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+filehandler = logging.FileHandler('organizer.log')
+filehandler.setFormatter(formatter)
+
+streamhandler = logging.StreamHandler()
+streamhandler.setFormatter(formatter)
+
+logger.addHandler(streamhandler)
+logger.addHandler(filehandler)
+
 
 UserInput = input('enter the name of the user who has the target folder in its homedirectory\n')
 PathInput = input('provide path to the folder you want to organize\n')
 CheckDir = os.listdir(f'/Users/{UserInput}')
-
-if PathInput in CheckDir:
-    PATH = f'/Users/{UserInput}/{PathInput}'
-    PathList = os.listdir(PATH)
-else:
-    print('Wrong input, please try again.')
 OutputDir = f'/Users/{UserInput}/file-maintenance-scripts-output'
 Documents = f'/Users/{UserInput}/file-maintenance-scripts-output/Documents'
 Pictures = f'/Users/{UserInput}/file-maintenance-scripts-output/Pictures'
 Videos = f'/Users/{UserInput}/file-maintenance-scripts-output/Videos'
+if PathInput in CheckDir:
+    PATH = f'/Users/{UserInput}/{PathInput}'
+    PathList = os.listdir(PATH)
+else:
+    logging.warning('Wrong input, please try again.')
 
 if not os.path.exists(OutputDir):
     os.makedirs(OutputDir)
-    print(f"Directory '{OutputDir}' created successfully.")
+    logging.info(f"Directory '{OutputDir}' created successfully.")
     os.makedirs(Videos)
-    print(f"Directory '{Videos}' created successfully.")
+    logging.info(f"Directory '{Videos}' created successfully.")
     os.makedirs(Pictures)
-    print(f"Directory '{Pictures}' created successfully.")
+    logging.info(f"Directory '{Pictures}' created successfully.")
     os.makedirs(Documents)
-    print(f"Directory '{Documents}' created successfully.")
+    logging.info(f"Directory '{Documents}' created successfully.")
 
 
 if not os.path.exists(Videos):
     os.makedirs(Videos)
-    print(f"Directory '{Videos}' created successfully.")
+    logging.info(f"Directory '{Videos}' created successfully.")
 
 
 if not os.path.exists(Pictures):
     os.makedirs(Pictures)
-    print(f"Directory '{Pictures}' created successfully.")
+    logging.info(f"Directory '{Pictures}' created successfully.")
 
 
 if not os.path.exists(Documents):
     os.makedirs(Documents)
-    print(f"Directory '{Documents}' created successfully.")
+    logging.info(f"Directory '{Documents}' created successfully.")
 
 
 def sorting(regex_pattern, file):
+
     if re.search(regex_pattern, file):
+
         if regex_pattern == "\.pdf" or regex_pattern == "\.docx":
             SourcePath = f"{PATH}/{file}" 
             DestinationPath = f'{Documents}/{file}'
-            print(f'{SourcePath}')
-            print(f'{DestinationPath}')
             shutil.move(SourcePath, DestinationPath)
+            logging.debug(f'{SourcePath} has been moved to: {DestinationPath}')
+
         if regex_pattern == "\.png" or regex_pattern == "\.jpg":
             SourcePath = f"{PATH}/{file}" 
             DestinationPath = f'{Pictures}/{file}'
-            print(f'{SourcePath}')
-            print(f'{DestinationPath}')
             shutil.move(SourcePath, DestinationPath)
+            logging.debug(f'{SourcePath} has been moved to: {DestinationPath}')
+
         if regex_pattern == "\.mp4":
             SourcePath = f"{PATH}/{file}" 
             DestinationPath = f'{Videos}/{file}'
-            print(f'{SourcePath}')
-            print(f'{DestinationPath}')
             shutil.move(SourcePath, DestinationPath)
+            logging.debug(f'{SourcePath} has been moved to: {DestinationPath}')
+
+
 for file in PathList:
     sorting(r'\.pdf', file)
     sorting(r'\.docx', file)
