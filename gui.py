33ee7bbc1a677b2e56d2cdbf4e218a3ca681f74c
Damian Walendzik <Damianwalendzik7@gmail.com>
2023-12-10 12:33:14 +0100

    pyqt script upgrade && starting to reorganize main.py

--- gui.py
+++ gui.py
@@ -1,9 +1,19 @@
+import typing
 from PyQt6.QtWidgets import *
-from PyQt6 import uic
+from PyQt6 import QtCore, uic
+from PyQt6.QtWidgets import QWidget
+
+class MyGUI(QMainWindow):
+
+    def __init__(self):
+        super(MyGUI, self).__init__()
+        uic.loadUi("organizerGUI.ui", self)
+        self.show()
 
 def main():
-    pass
-
+    app = QApplication([])
+    window = MyGUI()
+    app.exec()
 
 
 if __name__ == '__main__':
